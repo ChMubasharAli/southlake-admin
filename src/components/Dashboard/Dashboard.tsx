ba4c76985d482a2c873c0c3453f3b302ab346264
ChMubasharAli <muhammadmubashirali075@gmail.com>
2024-12-03 20:55:26 +0500

    updated code

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Welcome to Dashboard</h1>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Welcome to Dashboard</h1>
     </div>
   );
 }
